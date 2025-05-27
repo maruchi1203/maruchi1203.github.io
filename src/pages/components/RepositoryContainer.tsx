b7acd679c726978964f85cf3b9e6c33e54fdaece
JHeo <jheo1203@gmail.com>
2025-05-27 22:40:04 +0900

    feat(repositorycontainer.tsx의 github 리포지토리 카드 목록 로직 구현): 리포지토리 카드 목록 구현 및 썸네일 데이터 로직 및 렌더링 로직 변경
    새롭게 추가한 Icon에 따라서 레포지토리의 목록을 변경할 수 있게됨

--- src/pages/components/RepositoryContainer.tsx
+++ src/pages/components/RepositoryContainer.tsx
@@ -1,6 +1,7 @@
 import theme from "@src/styles/theme";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 interface Repo {
   id: number;
@@ -30,27 +31,86 @@ const Wrapper = styled.div`
   background-color: ${theme.lightColors.primary};
 `;
 
-const RepoCardContainer = styled.div`
+const RepoTitleContainer = styled.div`
   ${theme.flex.flexRow}
+  align-items: center;
+  font-size: larger;
+
+  gap: 10px;
+
+  .logo {
+    width: 36px;
+    height: auto;
+  }
+`;
+
+const RepoCardContainer = styled.div`
+  flex-grow: 1;
+  ${theme.flex.flexRow}
+  position: relative;
+  display: inline-block;
+
+  align-items: center;
   padding: 10px;
 
   width: 100%;
+  height: 400px;
+
+  :hover {
+    cursor: pointer;
+  }
 `;
 
-const RepoCard = styled.div`
-  ${theme.flex.flexCol}
-  align-items: start;
-  padding: 20px;
+const IconButtonContainer = styled.div<{ $visible: boolean }>`
+  flex-grow: 0;
 
-  width: 400px;
-  height: 300px;
+  > * {
+    visibility: ${(props) => (props.$visible ? "visible" : "hidden")};
+  }
+`;
+
+const RepoCard = styled.div<{ $renderedindex: number }>`
+  display: ${(props) =>
+    Math.abs(props.$renderedindex) <= 2 ? "block" : "none"};
+  z-index: ${(props) => `${100 - Math.abs(props.$renderedindex)}`};
+  scale: ${(props) => `${1 - 0.1 * Math.abs(props.$renderedindex)}`};
+
+  position: absolute;
+  align-items: start;
+
+  width: 500px;
+  height: 350px;
+
+  top: 50%;
+  left: ${(props) => `${50 + 10 * props.$renderedindex}%`};
+  transform: translate(-50%, -50%);
+  transition: transform 4s scale 4s z-index 4s;
 
   background-color: ${theme.lightColors.primary};
 
   box-shadow: 0 10px 10px ${theme.lightColors.secondary};
 
+  a {
+    ${theme.flex.flexCol}
+    align-items: start;
+    padding: 20px;
+
+    width: 100%;
+    height: 100%;
+  }
+
   span {
+    display: inline-block;
+    width: 100%;
     height: 30px;
+  }
+
+  .name {
+    font-size: x-large;
+  }
+
+  .description {
+    color: ${theme.lightColors.secondary};
   }
 `;
 
@@ -60,9 +120,18 @@ const RepoCard = styled.div`
 function RepositoryContainer(props: RepositoryContainerProps) {
   const { username } = props;
 
+  const [leftIconVisibility, SetLeftIconVisibility] = useState(false);
+  const [rightIconVisibility, SetRightIconVisibility] = useState(true);
+
   const [repos, setRepos] = useState<Repo[]>([]);
+  const [selectedRepo, SetSelectedRepo] = useState<number>(0);
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+
+  const repoRefs = useRef<Map<number, HTMLDivElement | null>>(new Map());
+  const leftArrow = document.getElementById("left-arrow");
+  const rightArrow = document.getElementById("right-arrow");
 
   const token = import.meta.env.VITE_GITHUB_TOKEN;
 
@@ -107,6 +176,33 @@ function RepositoryContainer(props: RepositoryContainerProps) {
     fetchRepos();
   }, [username]);
 
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      changeRepoCard();
+    });
+  }, [repos, selectedRepo]);
+
+  const leftIconOnClick = () => {
+    SetSelectedRepo((prev) => {
+      if (selectedRepo > 0) return selectedRepo - 1;
+      return prev;
+    });
+  };
+
+  const rightIconOnClick = () => {
+    SetSelectedRepo((prev) => {
+      if (selectedRepo < repos.length - 1) return selectedRepo + 1;
+      return prev;
+    });
+  };
+
+  const changeRepoCard = () => {
+    if (leftArrow && rightArrow) {
+      SetLeftIconVisibility(selectedRepo > 0 ? true : false);
+      SetRightIconVisibility(selectedRepo < repos.length - 1 ? true : false);
+    }
+  };
+
   if (loading) {
     return <Wrapper>💁‍♂️로딩 중......</Wrapper>;
   }
@@ -116,23 +212,50 @@ function RepositoryContainer(props: RepositoryContainerProps) {
 
   return (
     <Wrapper>
-      <span className="text-xl flex gap-[10px] items-center">
-        <div className="w-8 h-8">
+      <RepoTitleContainer>
+        <div className="logo">
           <img src="/src/images/github.png" width="100%" height="auto" />
         </div>
         Github 리포지토리 목록
-      </span>
-      <RepoCardContainer>
-        {/* <RepoArwLeftBtn />
-      <RepoArwRightBtn /> */}
-        {repos.map((repo) => (
-          <RepoCard key={repo.id}>
-            <span className="text-xl font-extrabold">{repo.name}</span>
-            <span>{repo.description}</span>
-            {repo.md ? <img src={repo.md} width="auto" height="200px" /> : null}
-          </RepoCard>
-        ))}
-      </RepoCardContainer>
+      </RepoTitleContainer>
+      <div className="w-full flex items-center">
+        <IconButtonContainer
+          onClick={leftIconOnClick}
+          $visible={leftIconVisibility}
+        >
+          <ChevronLeftIcon id="left-arrow" width="64px" />
+        </IconButtonContainer>
+        <RepoCardContainer>
+          {repos.map((repo, index) => (
+            <RepoCard
+              key={repo.id}
+              ref={(el) => {
+                repoRefs.current.set(repo.id, el);
+              }}
+              $renderedindex={index - selectedRepo}
+            >
+              <a href={repo.html_url}>
+                <span className="name">{repo.name}</span>
+                <span className="description">{repo.description}</span>
+                {repo.md ? (
+                  <img
+                    className="thumbnail"
+                    src={repo.md}
+                    width="auto"
+                    height="200px"
+                  />
+                ) : null}
+              </a>
+            </RepoCard>
+          ))}
+        </RepoCardContainer>
+        <IconButtonContainer
+          onClick={rightIconOnClick}
+          $visible={rightIconVisibility}
+        >
+          <ChevronRightIcon id="right-arrow" width="64px" />
+        </IconButtonContainer>
+      </div>
     </Wrapper>
   );
 }
