96528867178b64b397caa27c4fb04908c0d1af1e
JHeo <jheo1203@gmail.com>
2025-05-30 15:16:28 +0900

    test(백엔드 실험): index.html 참조 주소가 틀려서 수정

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import path from "path";
 
 const app = express();
-const distPath = path.resolve(__dirname, "dist");
+const distPath = path.resolve(__dirname);
 const PORT = process.env.PORT || 4000;
 
 app.use(
