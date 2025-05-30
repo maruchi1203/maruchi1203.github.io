a12155765125fadd10124dcc87276e0899cbe413
JHeo <jheo1203@gmail.com>
2025-05-30 19:44:46 +0900

    test

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 app.use(
   cors({
     origin: [
-      "https://maruchi1203.github.io",
+      "https://maruchi1203.onrender.com/",
       "http://localhost:4173/",
       "http://localhost:5173/",
     ], // 정확하게 GitHub Pages 도메인만 허용
