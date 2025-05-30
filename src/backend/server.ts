598f574c6642c3789885aaf4ed2173ae3a4ad721
JHeo <jheo1203@gmail.com>
2025-05-30 14:52:52 +0900

    fix(백엔드 dist 로딩 로직 변경): backend에서 dist 생성하게 하는 방식으로  로직 변경

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import path from "path";
 
 const app = express();
-const distPath = path.resolve(__dirname, "../frontend/dist");
+const distPath = path.resolve(__dirname, "dist");
 const PORT = process.env.PORT || 4000;
 
 app.use(
