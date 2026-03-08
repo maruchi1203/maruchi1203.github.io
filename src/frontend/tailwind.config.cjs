cbe758d9fda8de429dcd43d7b5271e067ff073ef
maruchi1203 <jheo1203@gmail.com>
2026-03-08 15:44:05 +0900

    수정 : 배포 설정 및 tailwind 설정 수정(미디어쿼리)

--- src/frontend/tailwind.config.cjs
+++ src/frontend/tailwind.config.cjs
@@ -7,7 +7,17 @@ module.exports = {
     "./app/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: "420px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "touch-only": { raw: "(hover: none) and (pointer: coarse)" },
+      "mouse-only": { raw: "(hover: hover) and (pointer: fine)" },
+      "max-md": { max: "767px" },
+    },
   },
   plugins: [],
 };
